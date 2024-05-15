a4160e370320a7e2c9c4f8ccbd9355c03b567118
Jono <84429457+jwono@users.noreply.github.com>
2024-05-15 15:46:33 +1000

    Added getComputerChoice function to randomly select rock, paper, or scissors

--- script.js
+++ script.js
@@ -1,1 +1,11 @@
 console.log("Hello World");
+
+function getComputerChoice() {
+    const choices = ["rock", "paper", "scissors"];
+    const randomIndex = Math.floor(Math.random() * choices.length);
+    return choices[randomIndex];
+}
+
+for (let i = 0; i < 10; i++) {
+    console.log(getComputerChoice());
+}
